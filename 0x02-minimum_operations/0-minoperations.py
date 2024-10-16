51c90309ebfeb51a4c0938ec05f49ee07b65707e
dhokabeatz <amglna2020@gmail.com>
2024-10-16 11:40:59 -0400

    All files lintered

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
+
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
